150dcecd4cf7f051650a126b431f76d0d7e13e5a
calloc134 <115209243+calloc134@users.noreply.github.com>
2023-09-01 21:28:26 +0900

    feat: 管理者以外の利用を禁止する処理の導入

--- src/index.ts
+++ src/index.ts
@@ -21,18 +21,73 @@ const POST_COMMAND = {
   ],
 };
 
-// リクエストの形式の定義
-type Request = {
-  body: {
-    type: InteractionType;
-    data: {
-      name: string;
-      options: {
-        name: string;
-        value: string;
-      }[];
+type ObjectType = {
+  app_permissions: string; // アプリの権限（文字列）
+  application_id: string; // アプリケーションのID（文字列）
+  channel: {
+    flags: number; // チャンネルに関連するフラグ（数値）
+    guild_id: string; // ギルド（サーバー）のID（文字列）
+    id: string; // チャンネル自体のID（文字列）
+    last_message_id: string; // 最後に送信されたメッセージのID（文字列）
+    name: string; // チャンネルの名前（文字列）
+    nsfw: boolean; // NSFW（Not Safe for Work）かどうか（真偽値）
+    parent_id: string; // 親カテゴリのID（文字列）
+    permissions: string; // チャンネルの権限（文字列）
+    position: number; // チャンネルの位置（数値）
+    rate_limit_per_user: number; // ユーザーごとのレート制限（数値）
+    topic: null | string; // チャンネルのトピック（nullまたは文字列）
+    type: number; // チャンネルのタイプ（数値）
+  };
+  channel_id: string; // チャンネルのID（文字列）
+  data: {
+    id: string; // データ項目のID（文字列）
+    name: string; // データ項目の名前（文字列）
+    options: any[]; // オプション（不明な型の配列）
+    type: number; // データ項目のタイプ（数値）
+  };
+  entitlement_sku_ids: any[]; // エンタイトルメントSKUのID（不明な型の配列）
+  entitlements: any[]; // エンタイトルメント（不明な型の配列）
+  guild: {
+    features: any[]; // ギルド（サーバー）の特徴（不明な型の配列）
+    id: string; // ギルド（サーバー）のID（文字列）
+    locale: string; // ギルド（サーバー）のロケール（言語設定、文字列）
+  };
+  guild_id: string; // ギルド（サーバー）のID（文字列）
+  guild_locale: string; // ギルド（サーバー）のロケール（言語設定、文字列）
+  id: string; // オブジェクト自体のID（文字列）
+  locale: string; // ロケール（言語設定、文字列）
+  member: {
+    avatar: null | string; // メンバーのアバター（nullまたは文字列）
+    communication_disabled_until: null | string; // コミュニケーションが無効になる時間（nullまたは文字列）
+    deaf: boolean; // メンバーが音声を聞こえない設定か（真偽値）
+    flags: number; // メンバーに関するフラグ（数値）
+    joined_at: string; // メンバーが参加した時間（文字列）
+    mute: boolean; // メンバーがミュートされているか（真偽値）
+    nick: null | string; // メンバーのニックネーム（nullまたは文字列）
+    pending: boolean; // メンバーの承認が保留中か（真偽値）
+    permissions: string; // メンバーの権限（文字列）
+    premium_since: null | string; // プレミアム会員になった日（nullまたは文字列）
+    roles: any[]; // メンバーの役割（不明な型の配列）
+    unusual_dm_activity_until: null | string; // 異常なDM活動が検出されるまでの時間（nullまたは文字列）
+    user: {
+      avatar: string; // ユーザーのアバター（文字列）
+      avatar_decoration_data: null | any; // アバターの装飾データ（nullまたは不明な型）
+      discriminator: string; // ユーザーの識別子（文字列）
+      global_name: string; // ユーザーのグローバル名（文字列）
+      id: string; // ユーザーのID（文字列）
+      public_flags: number; // ユーザーに関する公開フラグ（数値）
+      username: string; // ユーザー名（文字列）
     };
   };
+  token: string; // トークン（文字列）
+  type: InteractionType; // オブジェクトのタイプ（数値）
+  version: number; // バージョン（数値）
+};
+
+
+// リクエストの形式の定義
+type Request = {
+  body: ObjectType;
 };
 
 // 環境変数CHANNEL_IDSの取得
@@ -115,21 +170,31 @@ async function main() {
       switch (message.data.name) {
         // もし、コマンドの種類がPOSTだった場合は投稿の処理を行う
         case POST_COMMAND.name: {
-          // オプションを確認
-          console.debug("message", message);
-
           // 投稿内容を取得
           const content = message.data.options.find(
             (option) => option.name === POST_COMMAND.options[0].name
           )?.value;
 
-          // もし投稿元チャンネルが指定されたものでない場合は400エラーを返す
-          
+          // もし投稿者が指定されたユーザでない場合は400エラーを返す
+          if (message.member.user.id !== process.env.USER_ID) {
+            server.log.error("User is not allowed");
+            return response.status(400).send({
+              type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+              data: {
+                content: `投稿者が不正です`
+              },
+            });
+          }
 
           // 投稿内容がない場合は400エラーを返す
           if (!content) {
             server.log.error("Content is empty");
-            return response.status(400).send({ error: "Content is empty" });
+            return response.status(400).send({
+              type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+              data: {
+                content: `投稿内容が空です`
+              },
+            });
           }
 
           // 複数チャンネルの投稿を行う
@@ -162,7 +227,12 @@ async function main() {
             server.log.error("Failed to post message");
             return response
               .status(500)
-              .send({ error: "Failed to post message" });
+              .send({
+                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                data: {
+                  content: `投稿に失敗しました: ${content}`
+                },
+              });
           }
 
           // 投稿に成功した場合は200を返す
@@ -176,12 +246,23 @@ async function main() {
         }
         default: {
           server.log.error("Unknown Command");
+          response.status(400).send({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: {
+              content: `不明なコマンドです`
+            },
+          });
         }
       }
     } else {
       server.log.error("Unknown Type");
 
-      response.status(400).send({ error: "Unknown Type" });
+      response.status(400).send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: `不明なタイプです`
+        },
+      });
     }
   });
 
